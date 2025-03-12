265fd56ccc30a3afff81b9897452e0446323446c
Risidu <geeganagerisidu@gmail.com>
2025-03-12 15:13:38 +0530

    fhefjs

--- gitc.cpp
+++ gitc.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    int num1, num2, sum;
+    int num1, num2, diff;
 
     // Asking for user input
     std::cout << "Enter first number: ";
@@ -11,10 +11,10 @@ int main() {
     std::cin >> num2;
 
     // Calculating sum
-    sum = num1 + num2;
+    diff = num1 - num2;
 
     // Displaying result
-    std::cout << "Sum: " << sum << std::endl;
+    std::cout << "diff: " << diff << std::endl;
 
     return 0;
 }
